870c5be6691e72c894931e06fca3881c0afc6071
Antoine LRK <contact@antoineleroy.fr>
2022-05-09 12:40:37 +0200

    Remove EmojisBugged function

--- src/Console.js
+++ src/Console.js
@@ -1,6 +1,7 @@
 const sendMessage = (message, status) => {
     const defaultStatus = `➡️\u0020`;
-    status = (status != null ? (isBuggedEmoji(status) ? status = `${status}\u0020` : status) : status = defaultStatus);
+    status = (status != null ? status : status = defaultStatus);
+    // status = (status != null ? (isBuggedEmoji(status) ? status = `${status}\u0020` : status) : status = defaultStatus);
     let rawDate = new Date();
     const dateFormatted = `${setDate(rawDate.getDate())}/${setDate(rawDate.getMonth())}/${setDate(rawDate.getFullYear())}`;
     const tileFormatted = `${setDate(rawDate.getHours())}:${setDate(rawDate.getMinutes())}:${setDate(rawDate.getSeconds())}`;
@@ -11,10 +12,10 @@ const setDate = (value) => {
     return (value > 9 ? value : '0' + (value == 0 ? value + 1 : value));
 }
 
-const isBuggedEmoji = (value) => {
-    let emojies = ['✔️', '⚗️', '➡️', '⚙️'];
-    return emojies.filter(emo => emo == value).length > 0;
-}
+// const isBuggedEmoji = (value) => {
+//     let emojies = ['✔️', '⚗️', '➡️', '⚙️'];
+//     return emojies.filter(emo => emo == value).length > 0;
+// }
 
 export default {
     sendMessage
